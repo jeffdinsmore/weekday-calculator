0d6d099ecf695850ebb4db5acaf9eca373ec1281
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-03 09:22:10 -0700

    Added test to check if 1969 is subtracted from inputted year and test had a good fail
    Co-authored-by: Drew Parker <drewjparker91@gmail.com>

--- _tests_/weekday.test.js
+++ _tests_/weekday.test.js
@@ -1,33 +1,36 @@
 import WeekdayChecker from '../src/js/weekday.js';
 
 describe('WeekdayChecker', () => {
-
+  const weekdayChecker = new WeekdayChecker();
   test('should check if the inputted year is a leap year', () => {
-    const weekdayChecker = new WeekdayChecker();
     weekdayChecker.leapYear(2000);
     expect(weekdayChecker.leapYears).toBe(true);
   });
 
   test('should check if the inputted year is not a leap year', () => {
-    const weekdayChecker = new WeekdayChecker();
     weekdayChecker.leapYear(2001);
     expect(weekdayChecker.leapYears).toBe(false);
   });
 
   test('should check if the inputted day of month if greater than June, will add the days of the months greater than the set day of month', () => {
-    const weekdayChecker = new WeekdayChecker();
     weekdayChecker.monthCheck(12);
     expect(weekdayChecker.months).toEqual(183);
   });
 
   test('should check if the inputted day of month if smaller than June, will subtract the days of the months smaller than the set day of month', () => {
-    const weekdayChecker = new WeekdayChecker();
     weekdayChecker.monthCheck(4);
     expect(weekdayChecker.months).toEqual(-61);
   });
+
   test ('Should take inputted day and subtract 9 from it', () => {
-    const weekdayChecker = new WeekdayChecker();
     weekdayChecker.dayCheck(12);
     expect(weekdayChecker.days).toEqual(3);
   });
+
+  test ('Should take inputted year and if greater than, subtract 1969 from it', () => {
+    weekdayChecker.yearCheck(1971);
+    expect(weekdayChecker.years).toEqual(3);
+  });
+
+
 });
